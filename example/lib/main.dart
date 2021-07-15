7242ea566d1d1a8d4fafac335027949df53af52e
bradley <h65wang@uwaterloo.ca>
2021-07-15 14:37:10 -0700

    Add example project and improve description

---
+++ example/lib/main.dart
@@ -0,0 +1,75 @@
+import 'package:animated_flip_counter/animated_flip_counter.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  double _value = 0.0;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("AnimatedFlipCounter Demo"),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [
+            AnimatedFlipCounter(
+              value: 2000 + _value,
+            ),
+            AnimatedFlipCounter(
+              value: _value,
+              duration: Duration(seconds: 1),
+              textStyle: TextStyle(fontSize: 100, color: Colors.blue),
+            ),
+            AnimatedFlipCounter(
+              value: _value,
+              prefix: "Level ",
+              textStyle: TextStyle(
+                fontSize: 80,
+                fontWeight: FontWeight.bold,
+                letterSpacing: -8.0,
+                color: Colors.yellow,
+                shadows: [
+                  BoxShadow(
+                    color: Colors.orange,
+                    offset: Offset(2, 4),
+                    blurRadius: 4,
+                  ),
+                ],
+              ),
+            ),
+            AnimatedFlipCounter(
+              value: _value + 0.48,
+              fractionDigits: 2,
+            ),
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        child: Icon(Icons.add),
+        onPressed: () {
+          setState(() => _value += 1.0);
+        },
+      ),
+    );
+  }
+}
